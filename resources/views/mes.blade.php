e864413d068bb0f04f6d21636e852bf8af6b06eb
Unknown <marcoszaballos@gmail.com>
2019-02-20 20:01:51 +0100

    DOM: Insereix un node abans d'un altre node

--- resources/views/mes.blade.php
+++ resources/views/mes.blade.php
@@ -5,14 +5,14 @@
 @section("menu2")
 @endsection
 @section("content")
-
+<body onload="insertMessage()">
 <h1 class="text-center"> Coneix les tendes i restaurants del parc</h1>
 <h4 id="subtitol" class="text-center">No et pergues la <b>MILLOR</b> part del parc</h4>
-
+<p id="gelatilandia">Gelatilandia ...</p>
 <div id="div_taula">
 
 </div>
-
+</body>
 <!-- DOM CREATE TABLE -->
 <script>
     function tableCreate() {
@@ -29,6 +29,36 @@
     h4tag.style.fontSize = "32";
     h4tag.style.color = "darkgrey";
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- DOM: Insereix un node abans d'un altre node -->
+<script type="text/javascript">
+    function insertMessage() {
+        var newPara = document.createElement("p");
+        var newText = document.createTextNode("Univeypizza ...");
+        // If you copy this, don’t break the lines.
+        newPara.appendChild(newText);
+        var firstPara = document.getElementById("gelatilandia");
+        document.body.insertBefore(newPara, firstPara);
+    }
+</script>
+
 @endsection
 
 @section("footer")
