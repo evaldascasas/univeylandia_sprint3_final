6dd48ba38436153a1c88b4f6860ff123ae4bebec
Unknown <marcoszaballos@gmail.com>
2019-02-20 20:43:55 +0100

    <div id="TableContainer" align="center"></div>

--- resources/views/mes.blade.php
+++ resources/views/mes.blade.php
@@ -5,10 +5,26 @@
 @section("menu2")
 @endsection
 @section("content")
+<style type="text/css">
+
+    table {
+      width:380px;
+        border: 2px solid #4286f4; }
+    th { font-size: larger;
+        font-family:arial}
+    td { border: 1px solid #4286f4;
+        padding: 5px;
+        background-color:#ffffff;
+        font-family:arial;
+    }
+</style>
 <body onload="insertMessage()">
 <h1 class="text-center"> Coneix les tendes i restaurants del parc</h1>
 <h4 id="subtitol" class="text-center">No et pergues la <b>MILLOR</b> part del parc</h4>
-<p id="gelatilandia">Gelatilandia ...</p>
+<p id="gelatilandia"><b>Gelatilandia ...</b></p>
+
+<div id="TableContainer" align="center"></div>
+<br>
 <div id="div_taula">
   </div>
 <p id="para1">Avui es un gran dia per a estar al nostre parc d'atraccions ja que
@@ -57,8 +73,47 @@
         newPara.appendChild(newText);
         var firstPara = document.getElementById("gelatilandia");
         document.body.insertBefore(newPara, firstPara);
+
+
+        /* DOM: Crear una taula */
+
+        // Create the table elements
+        var Table = document.createElement("table");
+        Table.setAttribute("id","myTable"); // Create id
+        var THead = document.createElement("thead");
+        var TBody = document.createElement("tbody");
+        var Row, Cell;
+        var i, j;
+
+        // Insert the created elements into Table
+        Table.appendChild(THead);
+        Table.appendChild(TBody);
+
+        Row = document.createElement("tr");
+        TBody.appendChild(Row);
+
+        Cell = document.createElement("td");
+        Cell.innerHTML = "Foto 1";
+        Row.appendChild(Cell);
+        Cell = document.createElement("td");
+        Cell.innerHTML = "Foto 2";
+        Row.appendChild(Cell);
+
+        Row = document.createElement("tr");
+        TBody.appendChild(Row);
+        Cell = document.createElement("td");
+        Cell.innerHTML = "Foto 3";
+        Row.appendChild(Cell);
+        Cell = document.createElement("td");
+        Cell.innerHTML = "Foto 4";
+        Row.appendChild(Cell);
+
+        // Insert the table into the document tree
+        Tcontainer=document.getElementById("TableContainer");
+        Tcontainer.appendChild(Table);
     }
 </script>
+
 @endsection
 
 @section("footer")
