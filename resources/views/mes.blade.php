8398962831a7e9701ff3dd6f499a55f09936697b
Evaldas <evaldas_casas@hotmail.com>
2019-02-20 19:17:21 +0100

    mes.blade.php

--- resources/views/mes.blade.php
+++ resources/views/mes.blade.php
@@ -6,11 +6,6 @@
 @endsection
 @section("content")
 
-<div class="container jumbotron mt-3">
-    
-
-</div>
-
 <h1 class="text-center"> Coneix les tendes i restaurants del parc</h1>
 <h4 id="subtitol" class="text-center">No et pergues la <b>MILLOR</b> part del parc</h4>
 
@@ -18,17 +13,22 @@
 
 </div>
 
+<!-- DOM CREATE TABLE -->
 <script>
-function tableCreate() {
-  var div_taula = document.getElementById('div_taula')[0];
-  var taula = document.createElement('table');
-  var thead = taula.appendChild(document.createElement('thead'));
-}
-
-
-
+    function tableCreate() {
+        var div_taula = document.getElementById('div_taula')[0];
+        var taula = document.createElement('table');
+        var thead = taula.appendChild(document.createElement('thead'));
+    }
 </script>
 
+<!-- GET ELEMENT BY ID -->
+<script name="text/javascript">
+    var h4tag = document.getElementById("subtitol");
+    h4tag.style.fontFamily = "verdana";
+    h4tag.style.fontSize = "32";
+    h4tag.style.color = "darkgrey";
+</script>
 @endsection
 
 @section("footer")
